aa41edb80cc72dd9dce879c2999e997d20da5f01
ceka <cem.karaoguz@gmail.com>
2021-02-27 22:01:13 +0100

    Update train.py
    Made compatible with the new version of datasets

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 import torch
-from DataLoaders import RaceCarDataLoader
+from torch.utils.data import DataLoader, random_split
+from Datasets import CarRacingDataset
 from Models import ConvVAEWrapper, ResNetWrapper
 
-if __name__=="__main__":
+R_TRAIN = 0.7 # Ratio of training samples in dataset
 
+if __name__=="__main__":
+  
   im_width = 64
   im_height = 64
   
@@ -19,7 +22,7 @@ if __name__=="__main__":
           'in_features': im_height*im_width,
          }
 
-  args_resnet = {'num_epochs': 1,
+  args_resnet = {'num_epochs': 10,
           'lr': 0.01,
           'weight_decay': 1e-4,
           'grad_clip': 0.1,
@@ -32,18 +35,22 @@ if __name__=="__main__":
           'num_res_blocks': [3,3,3],
          }
          
-  #data_path = "./data_ai_small"
-  data_path = "./data_ai"
+  #data_path = "./data_ai"
+  data_path = "./data_ai_s"
+  #data_path = "./data_ai_m"
   batch_size = 64
 
-  # define the data loaders
-  train_loader = RaceCarDataLoader(data_path, batch_size=batch_size, shuffle=True, size=(im_height,im_width))
+  # Datasets and loaders
+  dataset = CarRacingDataset(data_path, size=(im_height,im_width))
+  train_set, valid_set = random_split(dataset, [int(len(dataset)*R_TRAIN), len(dataset)-int(len(dataset)*(R_TRAIN))])
+  train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+  valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=False)
 
   args_vae['steps_per_epoch'] = len(train_loader)
   args_resnet['steps_per_epoch'] = len(train_loader)
 
   resnet18 = ResNetWrapper(args_resnet)  
-  resnet18.train(train_loader)
+  resnet18.train(train_loader, valid_loader)
   
   '''
   ae = ConvVAEWrapper(args_vae)
