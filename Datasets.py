38ec6339a1a27004fb53a1c50479033cb733e53c
ceka <cem.karaoguz@gmail.com>
2021-03-02 23:14:15 +0100

    Update Datasets.py
    Can initialize with lists of samples directly

--- Datasets.py
+++ Datasets.py
@@ -4,13 +4,13 @@ from Utils import process_frame
 from torch.utils.data import Dataset
 from Globals import *
 import numpy as np
-
+  
 class CarRacingDataset(Dataset):
   
-  def __init__(self, root, size=(IM_HEIGHT, IM_WIDTH), 
+  def __init__(self, data_source, size=(IM_HEIGHT, IM_WIDTH), 
     crop=(IM_CROP_YMIN, IM_CROP_YMAX, IM_CROP_XMIN, IM_CROP_XMAX),
     num_skip_frames=50, action_space="discrete"):
-    self.root = root
+    self.data_source = data_source
     self.size = size
     self.crop = crop
     self.num_skip_frames = num_skip_frames
@@ -21,9 +21,21 @@ class CarRacingDataset(Dataset):
       self.process_action_vector = self._process_continuous_action
     else:
       raise(NotImplementedError)
-    self.action_histogram = np.zeros(NUM_DISCRETE_ACTIONS)
-    self.samples = self._load_files()
-    
+    self.action_histogram = np.zeros(NUM_DISCRETE_ACTIONS) 
+    if isinstance(data_source, str):
+      self.samples = self._load_files()
+    elif isinstance(data_source, list):
+      self.samples = []
+      for sample in data_source:
+        frame = process_frame(sample[0], self.crop, self.size)
+        action = self.process_action_vector(sample[2])
+        reward = sample[1]
+        self.samples.append((frame, reward, action))
+        if self.action_space=="discrete":
+          self.action_histogram[action]+=1
+    else:
+      raise(NotImplementedError)
+  
   def __len__(self):
     return len(self.samples)
     
@@ -32,9 +44,9 @@ class CarRacingDataset(Dataset):
         
   def _load_files(self):
     data = []
-    for file in os.listdir(self.root):
+    for file in os.listdir(self.data_source):
       if file.endswith(".pkl"):
-        data.extend(self._read_file(os.path.join(self.root, file)))
+        data.extend(self._read_file(os.path.join(self.data_source, file)))
     return data
 
   def _read_file(self, file_path):
@@ -47,38 +59,22 @@ class CarRacingDataset(Dataset):
         frame = process_frame(sample[0], self.crop, self.size)
         action = self.process_action_vector(sample[2])
         reward = sample[1]
-        
-        do_include_sample = True
-        '''
-        # For balancing dataset, not necessary if weights are used in the loss function
-        if(action_idx==0):
-          if np.random.rand()<0.2:
-            do_include_sample = True
-          else:
-            do_include_sample = False
-        
-        elif(action_idx==4):
-          if np.random.rand()<0.6:
-            do_include_sample = True
-          else:
-            do_include_sample = False
-        '''
-        if do_include_sample:
-          data_all.append((frame, reward, action))
-          if self.action_space=="discrete":
-            self.action_histogram[action]+=1
+        data_all.append((frame, reward, action))
+        if self.action_space=="discrete":
+          self.action_histogram[action]+=1
           
     return data_all
     
   def _process_discrete_action(self, action_vector):
-    if action_vector[0]>0:
+    
+    if action_vector[2]>0:
+      action_idx = 3
+    elif action_vector[0]>0:
       action_idx = 2
     elif action_vector[0]<0:
       action_idx = 4
     elif action_vector[1]>0:
       action_idx = 1
-    elif action_vector[2]>0:
-      action_idx = 3
     else:
       action_idx = 0
     return action_idx
