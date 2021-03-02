db4e17a736bb4c80c2c7ecb213119d722a7ff82e
ceka <cem.karaoguz@gmail.com>
2021-03-02 23:16:06 +0100

    Update Utils.py
    frame processing function is improved

--- Utils.py
+++ Utils.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os, getopt, sys
 from pickle import Pickler, Unpickler
+from Globals import *
 
 class AverageMeter(object):
 
@@ -20,23 +21,27 @@ class AverageMeter(object):
     self.count += n
     self.avg = self.sum / self.count
   
-def getValueFromDict(indict, key, defaultVal=None):
+def getValueFromDict(indict, key, defaultVal="argrequired"):
   if key in indict.keys():
     return indict[key]
   else:
-    if defaultVal is None:
+    if defaultVal=="argrequired":
       raise KeyError
     else:
       return defaultVal
 
 def process_frame(frame, crop, size):
   obs = frame[crop[0]:crop[1], crop[2]:crop[3],:]
-  obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY)
-  obs = cv2.resize(obs, size)
-  #obs = ((1.0 - obs) * 255).round().astype(np.uint8)
-  #obs = (obs * 255).round().astype(np.uint8)    
-  #obs = np.transpose(obs, [2,0,1])
-  return np.reshape(obs, [1, size[0], size[1]])
+  if IM_CHANNELS==1:
+    obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY)
+    obs = cv2.resize(obs, size)
+    obs = np.reshape(obs, [IM_CHANNELS, size[0], size[1]])
+  elif IM_CHANNELS==3:
+    obs = cv2.resize(obs, size)
+    obs = np.transpose(obs, [2,0,1])
+  else:
+    raise(NotImplementedError)
+  return obs/IM_DEPTH_NORM
 
 def saveLogData(logdata, folder):
   filepath = os.path.join(folder, 'logdata.pkl')
@@ -55,19 +60,18 @@ def loadLogData(folder):
       return Unpickler(f).load()
       
 def parseArguments(argv):
-  long_option_mappings = {'data_folder=': ['data_folder', '[TRAIN] Data root directory'],
-                          'cp=': ['do_save_checkpoint', '[TRAIN] Do save checkpoint?'],
-                          'cp_folder=': ['checkpoint_folder', '[TRAIN] Checkpoint folder'],
-                          'cp_step=': ['checkpoint_step', '[TRAIN] Checkpoint saving interval'],
-                          'num_epochs=': ['num_epochs', '[TRAIN] Number of epochs to train policy network'],
-                          'num_epochs_vae=': ['num_epochs_vae', '[TRAIN] Number of epochs to train VAE'],
-                          'load_epoch=': ['load_epoch', '[TRAIN] Checkpoint epoch number to load'],
-                          'do_load_vae=': ['do_load_vae', '[TRAIN] Load VAE Model from disk?'],
-                          'model_file=': ['model_file', '[TRAIN/EVAL] Path to the PN model file to be loaded'],
-                          'vae_model_file=': ['vae_model_file', '[TRAIN/EVAL] Path to the VAE model file to be loaded'],
-                          'num_episode=': ['num_episode', '[EVAL] Number of episodes to evaluate an agent'],
-                          'render_mode=': ['render_mode', '[EVAL] True for render environment on screen'],
-                          'override=': ['num_frames_override', '[EVAL] Number of frames to override actions in the beginning'], 
+  long_option_mappings = {'data_folder='    : ['data_folder', '[TRAIN] Data root directory'],
+                          'cp='             : ['do_save_checkpoint', '[TRAIN] Do save checkpoint?'],
+                          'cp_folder='      : ['checkpoint_folder', '[TRAIN] Checkpoint folder'],
+                          'num_epochs='     : ['num_epochs', '[TRAIN] Number of epochs to train policy network'],
+                          'num_epochs_vae=' : ['num_epochs_vae', '[TRAIN] Number of epochs to train VAE'],
+                          'load_epoch='     : ['load_epoch', '[TRAIN] Checkpoint epoch number to load'],
+                          'do_load_vae='    : ['do_load_vae', '[TRAIN] Load VAE Model from disk?'],
+                          'model_file='     : ['model_file', '[TRAIN/EVAL] Path to the PN model file to be loaded'],
+                          'vae_model_file=' : ['vae_model_file', '[TRAIN/EVAL] Path to the VAE model file to be loaded'],
+                          'num_episode='    : ['num_episode', '[EVAL] Number of episodes to evaluate an agent'],
+                          'render_mode='    : ['render_mode', '[EVAL] True for render environment on screen'],
+                          'epsilon='        : ['epsilon', '[EVAL] Epsilon for epsilon-greedy action selection'], 
                           }
                           
   def print_arguments():
