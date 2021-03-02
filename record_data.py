868e0a6b2bfadbc35a0c609afb4b8b8d542b7250
ceka <cem.karaoguz@gmail.com>
2021-03-02 23:15:19 +0100

    Update record_data.py
    implemented command line arguments

--- record_data.py
+++ record_data.py
@@ -5,20 +5,23 @@ import os
 from datetime import datetime
 import pickle
 from Globals import *
+from Utils import parseArguments, getValueFromDict
+from pyglet.window import key
 
 env_name = "CarRacing-v0"
 env = gym.make(env_name)
 
-
 if __name__ == "__main__":
-  from pyglet.window import key
 
-  a = np.array([0.0, 0.0, 0.0, 0.0])
+  cmd_args = parseArguments(sys.argv[1:])
+  data_folder = str(getValueFromDict(cmd_args, 'data_folder', './data'))
+  num_episode = int(getValueFromDict(cmd_args, 'num_episode', 1))
+
+  a = np.array([0.0, 0.0, 0.0])
   
-  path_record = "./data"
   filename = "expert_data_human_"+datetime.now().strftime("%Y%m%d%H%M%S")+".pkl"
-  if not os.path.exists(path_record):
-    os.mkdir(path_record)
+  if not os.path.exists(data_folder):
+    os.mkdir(data_folder)
   
   def key_press(k, mod):
     global restart
@@ -53,8 +56,7 @@ if __name__ == "__main__":
   env.viewer.window.on_key_press = key_press
   env.viewer.window.on_key_release = key_release
 
-  num_episodes = 1
-  for _ in range(num_episodes):
+  for _ in range(num_episode):
     iscarstarted = False
     samples = []
     s = env.reset()
@@ -62,8 +64,8 @@ if __name__ == "__main__":
     steps = 0
     restart = False
     while True:
-      #print(a)
       s_new, r, done, info = env.step(a)
+      #print('action :', a)
       total_reward += r
       steps += 1
       if a[1]>0:
@@ -78,7 +80,7 @@ if __name__ == "__main__":
     print(f"You scored {total_reward}...")
     user_input = input("Do you want to save your trajectory?")
     if user_input.lower() in ["y", "yes"]:
-      with open(os.path.join(path_record,filename), 'ab') as fp:
+      with open(os.path.join(data_folder,filename), 'ab') as fp:
         pickle.dump(samples, fp)
   
   env.close()
