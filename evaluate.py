fd0b7a6d358828cd5d9905767203fc0adcad26c4
ceka <cem.karaoguz@gmail.com>
2021-03-02 23:17:31 +0100

    Update evaluate.py
    session recording support

--- evaluate.py
+++ evaluate.py
@@ -5,62 +5,78 @@ import numpy as np
 import random
 import os
 import sys
+import pickle
+from datetime import datetime
 from Globals import *
 
-def run_episode(env, encoder, model, render_mode=True, num_episode=5, seed=-1, num_frames_override=20):
+def run_episodes(env, encoder, model, render_mode=True, num_episode=5, seed=-1, record_folder=''):
 
   reward_list = []
   t_list = []
   crop = (IM_CROP_YMIN, IM_CROP_YMAX, IM_CROP_XMIN, IM_CROP_XMAX)
   size = (IM_HEIGHT, IM_WIDTH)
-  max_episode_length = 1000
+  max_episode_length = MAX_EPISODE_LENGTH
+  do_record = record_folder!=''
 
   if (seed >= 0):
     random.seed(seed)
     np.random.seed(seed)
     env.seed(seed)
-
+  
+  samples = []
   for episode in range(num_episode):
 
-    obs = env.reset()
+    state = env.reset()
     total_reward = 0.0
+    samples_episode = []
     for t in range(max_episode_length):
 
       if render_mode:
         env.render("human")
       
-      if t<num_frames_override:
-        action = ACTION_MAPPING[1,:]
-      else:
-        action = model.get_action(encoder, process_frame(obs, crop=crop, size=size))
-      obs, reward, done, info = env.step(action)
-
+      action = model.get_action(encoder, process_frame(state, crop=crop, size=size))
+      next_state, reward, done, info = env.step(action)
+      
+      if do_record:
+        samples_episode.append((state, reward, action))
+        
       total_reward += reward
-
+      state = next_state
+      
       if done:
         break
 
-    if render_mode:
-      print("total reward", total_reward, "timesteps", t)
+    print(f"Episode {episode}, total reward: {total_reward}")
+    
+    samples.extend(samples_episode)
     reward_list.append(total_reward)
     t_list.append(t)
 
+  if do_record:
+    filename = "eval_"+datetime.now().strftime("%Y%m%d%H%M%S")+".pkl"
+    if not os.path.exists(record_folder):
+      os.mkdir(record_folder)
+    with open(os.path.join(record_folder, filename), 'ab') as fp:
+      pickle.dump(samples, fp)
+    
   return reward_list, t_list
   
 if __name__=="__main__":
 
   cmd_args = parseArguments(sys.argv[1:])
   num_episode = int(getValueFromDict(cmd_args, 'num_episode', 1))
-  render_mode = str(getValueFromDict(cmd_args, 'render_mode', "True")).lower() in ["true", "1"]
+  render_mode = str(getValueFromDict(cmd_args, 'render_mode', 'True')).lower() in ['true', '1']
   num_frames_override = int(getValueFromDict(cmd_args, 'num_frames_override', 20))
-  model_file = str(getValueFromDict(cmd_args, 'model_file', ""))
+  model_file = str(getValueFromDict(cmd_args, 'model_file', ''))
+  vae_model_file = str(getValueFromDict(cmd_args, 'vae_model_file', ''))
+  epsilon = float(getValueFromDict(cmd_args, 'epsilon', 0.0))
+  record_folder = str(getValueFromDict(cmd_args, 'data_folder', ''))
 
   if model_file=="":
     print("model_file argument is necessary!")
     sys.exit(2)
   checkpoint_folder, checkpoint_file = os.path.split(model_file) 
 
-  vae_model_file = str(getValueFromDict(cmd_args, 'vae_model_file', ""))
   if vae_model_file=="":
     print("vae_model_file argument is necessary!")
     sys.exit(2)   
@@ -75,20 +91,16 @@ if __name__=="__main__":
               'num_latent_features': 32,
               'strides': [2, 2, 2, 2],
               'do_use_cuda': True,
-              'in_features': IM_HEIGHT*IM_WIDTH,
-         }
+             }
+             
   # Policy network arguments
   args_pn = {'num_epochs': 1,
-        'lr': 0.01,
-        'weight_decay': 1e-4,
-        'grad_clip': 0.1,
-        'steps_per_epoch': None,
-        'do_use_cuda': True,
-        'num_classes': NUM_DISCRETE_ACTIONS,
-        'in_channels' : args_vae['num_latent_features'],
-        'num_channels': 64,
-        'category_weights': None,
-       }
+             'do_use_cuda': True,
+             'num_classes': NUM_DISCRETE_ACTIONS,
+             'in_channels' : args_vae['num_latent_features'],
+             'num_channels': 64,
+             'epsilon': epsilon,
+            }
        
   env_name = "CarRacing-v0"
   env = gym.make(env_name)
@@ -99,8 +111,8 @@ if __name__=="__main__":
   vae = ConvVAEWrapper(args_vae)
   vae.load_checkpoint(vae_checkpoint_folder, vae_checkpoint_file)
   
-  reward_list, t_list = run_episode(env, vae, policy_network, render_mode=render_mode, 
-    num_episode=num_episode, seed=seed, num_frames_override=num_frames_override)
+  reward_list, t_list = run_episodes(env, vae, policy_network, render_mode=render_mode, 
+    num_episode=num_episode, seed=seed, record_folder=record_folder)
   
-  print(f"Average reward for {num_episode} evaluation(s) : {np.mean(reward_list)}")
+  print(f"Mean/std reward for {num_episode} evaluation(s) : {np.mean(reward_list)} {np.std(reward_list)}")
   
